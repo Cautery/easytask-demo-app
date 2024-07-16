f0b398053922d89e8fbeef52480debcdd41a8dac
Brandon <Cautery@users.noreply.github.com>
2024-07-16 15:41:08 -0700

    Migrate all remaining components to use modules

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,16 +1,11 @@
 import { Component, Input } from '@angular/core';
 
-import { TaskComponent } from "./task/task.component";
-import { NewTaskComponent } from "./new-task/new-task.component";
 import { TasksService } from './tasks.service';
-
 
 @Component({
     selector: 'app-tasks',
-    standalone: true,
-    imports: [TaskComponent, NewTaskComponent],
     templateUrl: './tasks.component.html',
-    styleUrl: './tasks.component.css'
+    styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
     @Input({ required: true }) userId!: string;
@@ -18,7 +13,7 @@ export class TasksComponent {
 
     isAddingTask = false;
 
-    constructor(private tasksService: TasksService) { }
+    constructor(private tasksService: TasksService) {}
 
     get selectedUserTasks() {
         return this.tasksService.getUserTasks(this.userId);
